7ee296464567b774b0a91236afcb95ed62b2d6ca
cngondo <devngondo@gmail.com>
2019-11-27 19:50:35 -0600

    chore: Setup frontend dependencies and serving from static locations
    Setub bootstrap, jquery and popper

--- app.js
+++ app.js
@@ -7,10 +7,23 @@ const path = require("path");
 const app = express();
 
 app.use(morgan("tiny"));
+app.use(express.static(path.join(__dirname, "/public"))); // Will look for all static files to serve
+app.use(
+  "/css",
+  express.static(path.join(__dirname, "/node_modules/bootstrap/dist/css"))
+);
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "/node_modules/bootstrap/dist/js"))
+);
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "/node_modules/jquery/dist/"))
+);
 
 app.get("/", function(req, res) {
   // res.send("Hello from my library app");
-  res.sendFile(path.join(__dirname, "/views/index.html"));
+  res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
 app.listen(3000, function() {
