f6ddf9b43dc27197d3da11bd61dcc1b366b653c5
cngondo <devngondo@gmail.com>
2019-11-27 19:00:52 -0600

    chore: Setup express and supporting modules
    Setup morgan, chalk and debug for debugging

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const chalk = require("chalk");
+const debug = require("debug")("app");
+const morgan = require("morgan"); // gives information about requests
+const path = require("path");
+
+const app = express();
+
+app.use(morgan("tiny"));
+
+app.get("/", function(req, res) {
+  // res.send("Hello from my library app");
+  res.sendFile(path.join(__dirname, "/views/index.html"));
+});
+
+app.listen(3000, function() {
+  debug(`listening on port ${chalk.green("3000")}`);
+});
