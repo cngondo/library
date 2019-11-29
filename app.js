6d9f541911c262c189b2357848a3f2a0b2bd50d1
cngondo <devngondo@gmail.com>
2019-11-29 16:16:55 -0600

    chore: Setup vscode prettier with airbnb style guide

--- app.js
+++ app.js
@@ -1,31 +1,22 @@
-const express = require("express");
-const chalk = require("chalk");
-const debug = require("debug")("app");
-const morgan = require("morgan"); // gives information about requests
-const path = require("path");
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan'); // gives information about requests
+const path = require('path');
 
 const app = express();
 
-app.use(morgan("tiny"));
-app.use(express.static(path.join(__dirname, "/public"))); // Will look for all static files to serve
-app.use(
-  "/css",
-  express.static(path.join(__dirname, "/node_modules/bootstrap/dist/css"))
-);
-app.use(
-  "/js",
-  express.static(path.join(__dirname, "/node_modules/bootstrap/dist/js"))
-);
-app.use(
-  "/js",
-  express.static(path.join(__dirname, "/node_modules/jquery/dist/"))
-);
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public'))); // Will look for all static files to serve
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist/')));
 
-app.get("/", function(req, res) {
+app.get('/', (req, res) => {
   // res.send("Hello from my library app");
-  res.sendFile(path.join(__dirname, "views", "index.html"));
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-app.listen(3000, function() {
-  debug(`listening on port ${chalk.green("3000")}`);
+app.listen(3000, () => {
+  debug(`listening on port ${chalk.green('3000')}`);
 });
