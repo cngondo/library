4109f931c702a97be574870dcdf128aff3b262fa
cngondo <devngondo@gmail.com>
2020-01-13 21:38:22 -0600

    feat: Add encaspulated router for all book routes

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const bookRouter = express.Router(); // Conglomerates all routers to avoid repetition
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public'))); // Will look for all static files to serve
@@ -15,6 +16,17 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist/')
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+// All routers related to book
+bookRouter.route('/').get((req, res) => {
+  res.send('Hello books');
+});
+
+bookRouter.route('/singe', (req, res) => {
+  res.send('Hello single book!');
+});
+
+// Encapsulates all book router routes to a single route
+app.use('/books', bookRouter);
 app.get('/', (req, res) => {
   // res.send("Hello from my library app");
   // res.sendFile(path.join(__dirname, 'views', 'index.html'));
