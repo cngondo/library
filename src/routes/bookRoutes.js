9aa5e9a323049f73a44d3e389d95af8d402354e2
cngondo <devngondo@gmail.com>
2020-02-20 21:36:40 -0600

    feat(bookView): Display data from the database
    Setup UI to read correct KV pair from db

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bookRouter = express.Router(); // Conglomerates all routers to avoid repetition
+const sql = require('mssql');
+const debug = require('debug')('app:bookRoutes');
 
 function router(nav) {
   const books = [
@@ -37,11 +39,20 @@ function router(nav) {
 
   // All routers related to book
   bookRouter.route('/').get((req, res) => {
-    res.render('bookListView', {
-      title: 'My Library',
-      nav,
-      books
-    });
+    const request = new sql.Request();
+    request
+      .query('select * from books')
+      .then(results => {
+        debug(results);
+        res.render('bookListView', {
+          title: 'My Library',
+          nav,
+          books: results.recordset
+        });
+      })
+      .catch(err => {
+        debug(err);
+      });
   });
 
   // Pass the book id to the single route
